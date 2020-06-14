583d5ac50cbe712149eda35823a40710d05d5d4a
aquibmax <mdaquib327@gmail.com>
2020-06-14 14:02:18 +0530

    fetch api added

---
+++ routes/paytm/getUpcomingMovies.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+module.exports = async () => {
+  try {
+    let url = "https://apiproxy.paytm.com/v2/movies/upcoming";
+    const response = await axios.get(url);
+
+    return {
+      success: true,
+      data: response.data.upcomingMovieData,
+    };
+  } catch (err) {
+    return {
+      success: false,
+      msg: "Something went wrong",
+    };
+  }
+};
